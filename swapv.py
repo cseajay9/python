1f384668475985ea888a91be54f5b0a2d7b66e1e
cseajay9 <cseajay9@gmail.com>
2021-07-04 21:26:51 +0545

    Add files via upload

---
+++ swapv.py
@@ -0,0 +1,13 @@
+a=5
+b=7
+print("before swapping: ")
+print("a =",a,"b = ",b)
+b,a=a,b
+print("after swap ")
+print("a= ",a,"b = ",b)
+''' traditionally  we can also do this as folllow:
+    a = a+b
+    b=a-b
+    a=a-b
+    
+'''
